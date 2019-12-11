c534311396bb564ce54b00fbf59c78266058232b
Kurdiansyah Lesmana <kurdilesmana@gmail.com>
2019-12-11 22:04:00 +0700

    update sidebar menu

--- application/views/_layouts/sidebar.php
+++ application/views/_layouts/sidebar.php
@@ -9,14 +9,7 @@
 	</a>
 
 	<!-- Divider -->
-	<hr class="sidebar-divider my-0">
-
-	<!-- Nav Item - Dashboard -->
-	<li class="nav-item active">
-		<a class="nav-link" href="index.html">
-			<i class="fas fa-fw fa-tachometer-alt"></i>
-			<span>Dashboard</span></a>
-	</li>
+	<hr class="sidebar-divider">
 
 	<!-- Query Header Menu -->
 	<?php $headerMenu = $this->db->get('user_header_menu')->result_array(); ?>
@@ -34,99 +27,53 @@
 
 			$this->db->select('*');
 			$this->db->from('user_menu');
-			$this->db->join('user_access_menu', 'user_menu.id = user_access_menu.id');
+			$this->db->join('user_access_menu', 'user_menu.id = user_access_menu.menu_id', 'inner');
 			$this->db->where('user_menu.header_id', $headerID);
-			$this->db->where('user_menu.is_active', 1);
+			$this->db->where('user_menu.parent_id', Null);
 			$this->db->where('user_access_menu.role_id', $roleID);
+			$this->db->where('user_menu.is_active', 1);
 			$this->db->order_by('no_order', 'ASC');
 			$menu = $this->db->get()->result_array();
 
+			foreach ($menu as $m) {
+				$parentID = $m['menu_id'];
 
-			?>
+				$this->db->select('*');
+				$this->db->from('user_menu');
+				$this->db->join('user_access_menu', 'user_menu.id = user_access_menu.menu_id', 'inner');
+				$this->db->where('user_menu.header_id', $headerID);
+				$this->db->where('user_menu.parent_id', $parentID);
+				$this->db->where('user_access_menu.role_id', $roleID);
+				$this->db->where('user_menu.is_active', 1);
+				$this->db->order_by('no_order', 'ASC');
+				$subMenu = $this->db->get()->result_array();
+
+				if ($subMenu) { ?>
+				<li class="nav-item <?php echo $this->uri->segment(1) == $m['url'] ? 'active' : '';  ?>">
+					<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+						<i class="<?php echo $m['icon']; ?>"></i>
+						<span><?php echo $m['title']; ?></span>
+					</a>
+					<div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+						<div class="bg-white py-2 collapse-inner rounded">
+							<?php foreach ($subMenu as $sm) { ?>
+								<a class="collapse-item <?php echo $title == $sm['title'] ? 'active' : '' ?>" href="<?php echo base_url($sm['url']); ?>"><?php echo $sm['title']; ?></a>
+							<?php } ?>
+						</div>
+					</div>
+				</li>
+			<?php } else { ?>
+				<li class="nav-item <?php echo $title == $m['title'] ? 'active' : '' ?>">
+					<a class="nav-link" href="<?php echo base_url($m['url']); ?>">
+						<i class="<?php echo $m['icon']; ?>"></i>
+						<span><?php echo $m['title']; ?></span></a>
+				</li>
+		<?php }
+			} ?>
+
+		<!-- Divider -->
+		<hr class="sidebar-divider my2">
 
 	<?php } ?>
-
-
-	<!-- Nav Item - Pages Collapse Menu -->
-	<li class="nav-item">
-		<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-			<i class="fas fa-fw fa-cog"></i>
-			<span>Components</span>
-		</a>
-		<div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-			<div class="bg-white py-2 collapse-inner rounded">
-				<h6 class="collapse-header">Custom Components:</h6>
-				<a class="collapse-item" href="buttons.html">Buttons</a>
-				<a class="collapse-item" href="cards.html">Cards</a>
-			</div>
-		</div>
-	</li>
-
-	<!-- Nav Item - Utilities Collapse Menu -->
-	<li class="nav-item">
-		<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
-			<i class="fas fa-fw fa-wrench"></i>
-			<span>Utilities</span>
-		</a>
-		<div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
-			<div class="bg-white py-2 collapse-inner rounded">
-				<h6 class="collapse-header">Custom Utilities:</h6>
-				<a class="collapse-item" href="utilities-color.html">Colors</a>
-				<a class="collapse-item" href="utilities-border.html">Borders</a>
-				<a class="collapse-item" href="utilities-animation.html">Animations</a>
-				<a class="collapse-item" href="utilities-other.html">Other</a>
-			</div>
-		</div>
-	</li>
-
-	<!-- Divider -->
-	<hr class="sidebar-divider">
-
-	<!-- Heading -->
-	<div class="sidebar-heading">
-		Addons
-	</div>
-
-	<!-- Nav Item - Pages Collapse Menu -->
-	<li class="nav-item">
-		<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
-			<i class="fas fa-fw fa-folder"></i>
-			<span>Pages</span>
-		</a>
-		<div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-			<div class="bg-white py-2 collapse-inner rounded">
-				<h6 class="collapse-header">Login Screens:</h6>
-				<a class="collapse-item" href="login.html">Login</a>
-				<a class="collapse-item" href="register.html">Register</a>
-				<a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-				<div class="collapse-divider"></div>
-				<h6 class="collapse-header">Other Pages:</h6>
-				<a class="collapse-item" href="404.html">404 Page</a>
-				<a class="collapse-item" href="blank.html">Blank Page</a>
-			</div>
-		</div>
-	</li>
-
-	<!-- Nav Item - Charts -->
-	<li class="nav-item">
-		<a class="nav-link" href="charts.html">
-			<i class="fas fa-fw fa-chart-area"></i>
-			<span>Charts</span></a>
-	</li>
-
-	<!-- Nav Item - Tables -->
-	<li class="nav-item">
-		<a class="nav-link" href="tables.html">
-			<i class="fas fa-fw fa-table"></i>
-			<span>Tables</span></a>
-	</li>
-
-	<!-- Divider -->
-	<hr class="sidebar-divider d-none d-md-block">
-
-	<!-- Sidebar Toggler (Sidebar) -->
-	<div class="text-center d-none d-md-inline">
-		<button class="rounded-circle border-0" id="sidebarToggle"></button>
-	</div>
 
 </ul>
